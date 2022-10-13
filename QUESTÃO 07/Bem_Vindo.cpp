624d416f51ea55093a50f48e1d669b408fdc813d
AllanOliveira2022 <115299780+AllanOliveira2022@users.noreply.github.com>
2022-10-13 13:30:29 -0300

    Resposta da questão 07

---
+++ QUESTÃO 07/Bem_Vindo.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+         setlocale(LC_ALL, "portuguese_brazil");
+         printf("####################\n");
+         printf("#                  #\n");
+         printf("#                  #\n");
+         printf("#                  #\n");
+         printf("#    BEM VINDO     #\n");
+         printf("#                  #\n");
+         printf("#                  #\n");
+         printf("#                  #\n");
+         printf("####################\n");
+  
+return 0;
+
+}
